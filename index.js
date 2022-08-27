59b4b140a541f10ebffbebad3c8315625b1529bf
madrodgerflynn <madrodgerflynn@gmail.com>
2022-08-26 21:23:55 -0600

    working on the index.js

--- index.js
+++ index.js
@@ -1,40 +1,45 @@
 // TODO: Include packages needed for this application
 
+//Installed package.json & package-lock.json
+
 // TODO: Create an array of questions for user input
 const questions = [
-    {
-        type: "input",
-        message: "Please provide a short description of your project.",
-        name: "description",
-    }
-    {
-        type: "checkbox",
-        message: "Would you like your README to include a table of contents?",
-        choices: ["yes", "no"],
-        name: "tOc",
-    }
-    {
-        type: "input",
-        message: "Please provide any steps needed to install your project. ",
-        name: "install",
-    }
-    {
-        type: "input",
-        message: "Please provide any instructions for use.",
-        name: "usage",
-    }
-    {
-        type: "checkbox",
-        message: "Please check the contribitors used in your project",
-        choices: ["Bootstrap", "Google Fonts", "GitHub"],
-        name: "description",
-    }
-    {
-        type: "input",
-        message: "Please provide a short description of your project",
-        name: "description",
-    }
-   
+  {
+    //  Description
+    type: "input",
+    message: "Please provide a short description of your project.",
+    name: "description",
+  },
+  {
+    // ToC
+    type: "checkbox",
+    message: "Would you like your README to include a table of contents?",
+    choices: ["yes", "no"],
+    name: "tOc",
+  },
+  {
+    //Installation
+    type: "input",
+    message: "Please provide any steps needed to install your project. ",
+    name: "install",
+  },
+  {
+    // Usage
+    type: "input",
+    message: "Please provide any instructions for use.",
+    name: "usage",
+  },
+  {
+    // Contributors
+    type: "input",
+    message: "Please provide any contributors that were used.",
+    name: "contributors",
+  },
+  {
+    type: "input",
+    message: "Please provide ",
+    name: "description",
+  },
 ];
 
 // TODO: Create a function to write README file
